129234428c6eefc03597a44857724716d7d24083
ManuelBanchero <manuelbanchero04@gmail.com>
2026-08-12 15:21:44 -0300

    basic cli is running

--- main.go
+++ main.go
@@ -25,12 +25,22 @@ func main() {
 
 	cmds.Register("login", commands.HandlerLogin)
 
-	fmt.Println(s.Cfg.CurrentUserName)
-
 	args := os.Args
 	if len(args) < 2 {
 		fmt.Println("Must include a command")
-		return
+		os.Exit(1)
 	}
 
+	commandName := args[1]
+	arguments := args[2:]
+
+	command := commands.Command{
+		Name: commandName,
+		Args: arguments,
+	}
+
+	if err := cmds.Run(&s, command); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
